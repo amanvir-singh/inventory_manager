b5f420a24250fc1cb1f82fa10acfb45fa3d28944
Amanvir Singh <amanvir.singh880@gmail.com>
2024-12-06 21:38:50 -0500

    Added Routes in App.js

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,12 @@ require('dotenv').config();
 
 const connectDB = require('./database');
 const usersRoutes = require('./routes/users');
+const finishesRoutes = require('./routes/finishes');
+const logsRoutes = require('./routes/logs');
+const materialsRoutes = require('./routes/materials');
+const suppliersRoutes = require('./routes/suppliers');
+const thicknessesRoutes = require('./routes/thicknessnes');
+
 
 const app = express();
 
@@ -15,6 +21,12 @@ app.use(express.json());
 
 // Routes
 app.use('/users', usersRoutes);
+app.use('./finishes', finishesRoutes);
+app.use('./logs', logsRoutes);
+app.use('./materials', materialsRoutes);
+app.use('./suppliers', suppliersRoutes);
+app.use('./thicknesses', thicknessesRoutes);
+
 
 
 const PORT = process.env.PORT || 3000;
